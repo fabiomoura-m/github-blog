80240370c38b2b595e9d63635b7bb05e983991d3
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2022-10-07 11:17:26 -0300

    feat: Header Component

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import headerBG from '../../assets/headerBg.png';
+
+export const HeaderContainer = styled.header`
+    width: 100%;
+    height: 18.5rem;
+    background: url(${headerBG}) no-repeat center;
+    background-size: cover;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
