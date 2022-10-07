3250e4b596632d745af020940cf8d8a378bf6f5a
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2022-10-07 10:22:27 -0300

    style: add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { Router } from './Router';
+import { GlobalStyle } from './styles/global';
+import { defaultTheme } from './styles/theme/defaultTheme';
+
 export function App() {
     return (
-        <div>
-            <h1>Hello</h1>
-        </div>
+        <BrowserRouter>
+            <ThemeProvider theme={defaultTheme}>
+                <GlobalStyle />
+                <Router />
+            </ThemeProvider>
+        </BrowserRouter>
     );
 }
