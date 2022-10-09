c78175afb7a8df7f19752383418e762c4a35fc28
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2022-10-09 19:14:17 -0300

    feat: Post Component

--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -1,11 +1,17 @@
+import { Post } from './components/Post';
 import { Profile } from './components/Profile';
 import { SearchInput } from './components/SearchInput';
+import { PostsListConainer } from './styles';
 
 export function Blog() {
     return (
         <div>
             <Profile />
             <SearchInput />
+            <PostsListConainer>
+                <Post />
+                <Post />
+            </PostsListConainer>
         </div>
     );
 }
