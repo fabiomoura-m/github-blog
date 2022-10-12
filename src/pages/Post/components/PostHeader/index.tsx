f85ccd31b20b3d42ed30c8d8909838ebc33cc9b9
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2022-10-12 20:00:52 -0300

    feat: iterative PostHeader component with github api

--- src/pages/Post/components/PostHeader/index.tsx
+++ src/pages/Post/components/PostHeader/index.tsx
@@ -8,38 +8,59 @@ import {
     faChevronLeft
 } from '@fortawesome/free-solid-svg-icons';
 import { useNavigate } from 'react-router-dom';
+import { IPost } from '../../../Blog';
+import { relativeDateFormatter } from '../../../../utils/formatter';
+import { Spinner } from '../../../../components/Spinner';
 
-export function PostHeader() {
+interface PostHeaderProps {
+    postData: IPost;
+    isLoading: boolean;
+}
+
+export function PostHeader({ postData, isLoading }: PostHeaderProps) {
     const navigate = useNavigate();
     function goBack() {
         navigate(-1);
     }
+    console.log(postData);
+    const formattedDate = relativeDateFormatter(postData.created_at);
     return (
         <PostHeaderContainer>
-            <header>
-                <ExternalLink
-                    as="button"
-                    icon={<FontAwesomeIcon icon={faChevronLeft} />}
-                    variant="iconLeft"
-                    text="Voltar"
-                    onClick={goBack}
-                />
-                <ExternalLink text="Ver no github" href="#" target="_blank" />
-            </header>
-            <h1>JavaScript data types and data structures</h1>
-            <ul>
-                <li>
-                    <FontAwesomeIcon icon={faGithub} />
-                    fabiomoura-m
-                </li>
-                <li>
-                    <FontAwesomeIcon icon={faCalendar} />
-                    Há 1 dia
-                </li>
-                <li>
-                    <FontAwesomeIcon icon={faComment} />5 comentários
-                </li>
-            </ul>
+            {isLoading ? (
+                <Spinner />
+            ) : (
+                <>
+                    <header>
+                        <ExternalLink
+                            as="button"
+                            icon={<FontAwesomeIcon icon={faChevronLeft} />}
+                            variant="iconLeft"
+                            text="Voltar"
+                            onClick={goBack}
+                        />
+                        <ExternalLink
+                            text="Ver no github"
+                            href={postData.html_url}
+                            target="_blank"
+                        />
+                    </header>
+                    <h1>{postData.title}</h1>
+                    <ul>
+                        <li>
+                            <FontAwesomeIcon icon={faGithub} />
+                            {postData.user.login}
+                        </li>
+                        <li>
+                            <FontAwesomeIcon icon={faCalendar} />
+                            {formattedDate}
+                        </li>
+                        <li>
+                            <FontAwesomeIcon icon={faComment} />
+                            {postData.comments} comentários
+                        </li>
+                    </ul>
+                </>
+            )}
         </PostHeaderContainer>
     );
 }
